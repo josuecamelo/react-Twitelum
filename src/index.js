162a6b129a39942e1f506913a07b35250e0624c4
Josué Camelo <josueprg@gmail.com>
2021-03-18 19:26:04 -0300

    React Router Dom

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Roteamento
+import { BrowserRouter } from "react-router-dom";
+import Roteamento from "./routes.js";
+
 // CSSs Globais
 import "./assets/css/reset.css";
 import "./assets/css/container.css";
@@ -12,10 +16,14 @@ import "./assets/css/notificacao.css";
 import "./assets/css/novoTweet.css";
 // import './index.css';
 
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import HomePage from "./pages/HomePage";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<BrowserRouter>
+        <Roteamento />
+    </BrowserRouter>,
+     document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
