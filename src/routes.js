5ce70f7cfa9464019a4584e5597e5c96b436e43a
Josué Camelo <josueprg@gmail.com>
2021-03-19 22:26:29 -0300

    Private Routes

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, {Component} from "react";
 import { Switch, Route } from "react-router-dom";
+
 // Páginas
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
-class Roteamento extends React.Component {
+import { Redirect } from 'react-router-dom'
+
+class PrivateRoute extends Component {
+    estaAutenticado = () => {
+        if(localStorage.getItem('TOKEN')) {
+            return true
+        } else {
+            return false
+        }
+    }
+
+    render() {
+        const { component: Component, ...props } = this.props
+        if(this.estaAutenticado()) {
+            return <Component {...props} />
+        } else {
+            return <Redirect to="/login" />
+        }
+    }
+}
+
+class Roteamento extends Component {
     render() {
         return (
             <Switch>
-                <Route path="/" component={HomePage} exact />
+                <PrivateRoute path="/" component={HomePage} exact />
                 <Route path="/login" component={LoginPage} />
             </Switch>
         );
