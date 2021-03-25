474e8c7a48a5d2e25af3c24e63f1078025a0e0b6
Josué Camelo <josueprg@gmail.com>
2021-03-25 18:58:25 -0300

    Redux Thunk

--- src/index.js
+++ src/index.js
@@ -17,7 +17,8 @@ import Roteamento from "./routes";
 import { BrowserRouter } from "react-router-dom";
 import { NotificacaoContextProvider } from "./contexts/NotificacaoContext";
 
-import store from "./store";
+import store from './store/store'
+
 import { Provider } from "react-redux";
 
 ReactDOM.render(
